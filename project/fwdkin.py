12c4c49cc21932065873ee29f733afc955864840
Ande <andyjma.work@gmail.com>
2023-11-30 16:42:06 -0500

    add invkin and fix fwdkin

--- project/fwdkin.py
+++ project/fwdkin.py
@@ -1,12 +1,12 @@
 import numpy as np
-from utils import euler_rodrigues, get_homogeneous
+from utils import rot, get_homogeneous
 from Robot import UR5Arm
 
 def fwdkin(arm: UR5Arm, theta: np.array)->tuple():
     """Calculates the forward kinematics of the DofBot
     Args:
         arm - Robot arm class with home config info and joint value
-        theta - the angles of rotation for each joint
+        theta - (5 x 1) the angles of rotation for each joint
     Returns:
         End effector matrix (4x4) 
     """
@@ -14,12 +14,12 @@ def fwdkin(arm: UR5Arm, theta: np.array)->tuple():
     H = arm.get_H()
     
     R_0T = np.eye(3)
-    P_0T = P[:,-1]
+    P_0T = P[:,0]
     
-    for i in range(np.shape(H)[1]-1, -1, -1):
-        R_ij = euler_rodrigues(H[:,i],theta[i])
-        # R_01 @ R_12 @ R_23 @ R_34 @ R_45
-        R_0T = R_ij @ R_0T
-        P_0T = P[:,i] + R_ij @ P_0T
+    for i in range(1, P.shape[1]):
+        R_ij = rot(H[:,i-1],theta[i-1,0])
+        
+        R_0T = R_0T @ R_ij
+        P_0T = P_0T + R_0T @ P[:,i]
 
     return get_homogeneous(R_0T, P_0T.reshape(3,1))
